fa747587dc97b04067d5b317b8816122123c5210
Watenehub <watenekeguti@gmail.com>
2026-09-20 18:23:13 +0300

    Add scroll-to-top on route change
    - Added ScrollToTop component that scrolls to top on pathname change
    - Ensures pages load at top on all devices (desktop, tablet, mobile)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import { AuthProvider } from './context/AuthContext'
 import Layout from './components/Layout'
 import Routes from './routes'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <AuthProvider>
         <Layout>
           <Routes />
